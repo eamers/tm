97b75f61f055984f381b7f43141a99caecf9c727
Brendan Eamer <brendan@eamer.ca>
2013-01-01 12:51:44 -0500

    Adds Tiny MCE test material

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tm::Application.config.secret_token = '7a35b2d75e6f63ef6f72d3e1dcc88d3baa61502149c5637d45b4a44b1fb798afd8dfcbd358da3720b44489dae52b72bb72e78a1b3f35d6280cfba8f38f41bf7b'
